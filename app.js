a4a9b4ae9f3706b3137148d93b68cf5dad585fc7
Ruth-Ann Blair <blair.ruthie1@gmail.com>
2022-11-06 20:13:42 -0500

    Added superhero lookup

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 window.addEventListener('load', ()=>{
-    document.querySelector("button#btn").addEventListener("click", ()=>{
-        fetch('http://localhost:8080/superheroes.php')
+    let heading = document.querySelector("header h1");
+
+    document.querySelector("button#btn").addEventListener("click", (event)=>{
+        event.preventDefault();
+        let searchVal = document.querySelector("input#searchField").value.replace(/[-&\/\\#,+()$@|~%!.'":;*?<>{}]/g,'');
+        let resultDiv = document.querySelector("div#result");
+
+        let cleanUrl = `http://localhost:8080/superheroes.php?query= ${searchVal}`.replace( /"[^-0-9+&@#/%?=~_|!:,.;\(\)]"/g,'');
+
+        fetch(cleanUrl, {method : 'GET'})
         .then(resp => resp.text())
-        .then(data => alert(data))
+        .then(elements => {
+            let h3Element = document.createElement("h3");
+            let h3Text = document.createTextNode("RESULT");
+            h3Element.appendChild(h3Text);
+            let hrElement= document.createElement("hr");
+            resultDiv.innerHTML = '';
+            resultDiv.innerHTML = elements;
+            resultDiv.prepend(h3Element, hrElement);
+        })
     });
+
+    document.querySelector("input#searchField").onblur = ()=>{
+        querySelector("input#searchField").style.borderColor = "#FF3232";
+    }
+
 });
